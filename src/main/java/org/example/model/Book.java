2b6e9fff93b1cc8a37bc9cb92da54b1f4ed98ceb
Iv00vI <goran.ivo11@gmail.com>
2025-05-14 21:10:31 +0200

    c

--- src/main/java/org/example/model/Book.java
+++ src/main/java/org/example/model/Book.java
@@ -1,4 +1,50 @@
 package org.example.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String title;
+
+
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
